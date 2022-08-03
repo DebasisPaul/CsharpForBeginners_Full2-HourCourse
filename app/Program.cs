7606570e247f4ed3142bdc528d119e3cbde8305a
debasispaul <idebasispaul@gmail.com>
2022-08-02 23:58:18 -0700

    String Added

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-string aFriend = "Scott";
-Console.WriteLine($"Hello {aFriend}");
+/*string aFriend = "Scott";
+Console.WriteLine($"Hello {aFriend}");*/
+
+// C#: The Basics of Strings
+string firstFriend = "Scott";
+string secondFriend = "Kendra";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name{firstFriend} has {firstfriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
