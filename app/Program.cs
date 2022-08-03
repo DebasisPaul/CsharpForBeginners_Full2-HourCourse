075e4f0db457e65e65044aabcf341ccb1d123f17
debasispaul <idebasispaul@gmail.com>
2022-08-03 05:52:43 -0700

    Working With String

--- app/Program.cs
+++ app/Program.cs
@@ -1,13 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+// =======================================================
+
 /*string aFriend = "Scott";
 Console.WriteLine($"Hello {aFriend}");*/
 
+// ========================================================
+
 // C#: The Basics of Strings
-string firstFriend = "Scott";
+// Multiple Comment Keyboard Shortcot command "Alt+Shift+A"
+/* string firstFriend = "Scott";
 string secondFriend = "Kendra";
 Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters."); */
 
-Console.WriteLine($"The name{firstFriend} has {firstfriend.Length} letters.");
-Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+// =========================================================
+
+//Do More With Strings
+/* string greeting = "      Hello World!      ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]"); */
+
+// ===========================================================
