9584334eab1cba31b505c89fb4af6ec7883d0b87
debasispaul <idebasispaul@gmail.com>
2022-08-03 19:22:08 -0700

    // More Working With Strings

--- app/Program.cs
+++ app/Program.cs
@@ -32,3 +32,13 @@ trimmedGreeting = greeting.Trim();
 Console.WriteLine($"[{trimmedGreeting}]"); */
 
 // ===========================================================
+
+/* string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Kendra");
+Console.WriteLine(sayHello);
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower()); */
+
+// ============================================================
