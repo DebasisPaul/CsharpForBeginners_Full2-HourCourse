448df108121cbe52fc0ea19a4552e7bd7f049c7f
debasispaul <idebasispaul@gmail.com>
2022-08-04 03:52:12 -0700

    // C#: Numbers and Decimals

--- app/Program.cs
+++ app/Program.cs
@@ -133,3 +133,43 @@ double min = double.MinValue;
 Console.WriteLine($"The range of double is {min} to {max}"); */
 
 
+/* double third = 1.0 / 3.0;
+Console.WriteLine(third); */
+
+// ===============================================================================
+
+// C#: Numbers and Decimals
+
+/* decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+
+Console.WriteLine($"The renge of the decimal type is {min} to {max}"); */
+
+/* double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+int min2 = int.MinValue;
+int max2 = int.MaxValue;
+Console.WriteLine($"The range of the int type is {min2} to {max2}");
+
+long min3 = long.MinValue;
+long max3 = long.MaxValue;
+Console.WriteLine($"The range of the long type is {min3} to {max3}");
+
+short min4 = short.MinValue;
+short max4 = short.MaxValue;
+Console.WriteLine($"The range of the short type is {min4} to {max4}");
+
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area); */
