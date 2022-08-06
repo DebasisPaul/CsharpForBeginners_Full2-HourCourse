8a9ef36e37a3852cad74df40446d8282e63068a7
debasispaul <idebasispaul@gmail.com>
2022-08-06 08:45:56 -0700

    // C#: Lists of Other Types

--- app/Program.cs
+++ app/Program.cs
@@ -503,3 +503,33 @@ else
             Console.WriteLine($"Hello {name.ToUpper()}!");
         }
     } */
+
+    // ================================================================
+
+    // C#: Lists of Other Types
+
+    using System;
+    using System.Collections.Generic;
+
+    namespace list_tutorial
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var fibonacciNumbers = new List<int> { 1, 1 };
+
+                while (fibonacciNumbers.Count < 20)
+                {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+                }
+
+                foreach (var item in fibonacciNumbers)
+                    Console.WriteLine(item);
+            }
+        }
+    
+    }
