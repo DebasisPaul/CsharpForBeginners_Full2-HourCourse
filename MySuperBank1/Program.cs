1466b736a50d98913080ef9c5a0ed88e1abfe8bc
debasispaul <idebasispaul@gmail.com>
2022-08-06 22:48:04 -0700

    New Project Added

---
+++ MySuperBank1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            account.Deposit(100);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+        }
+    }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -564,36 +564,3 @@ else
     
     }  */
 
-    // =================================================================================================================================
-
-    // C#: Objects Oriented Programming - Objects and Classes
-
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
-    namespace MySuperBank
-    {
-        class BankAccount
-        {
-            public string Number { get; }
-            public string Owner { get; set; }
-            public decimal Balance { get; }
-
-            public BankAccount(string name, decimal initialBalance)
-            {
-                this. Owner = name;
-                this.Balance  = initialBalance;
-            }
-
-            public void MakeDeposit(decimal amount, DateTime date, string note)
-            {
-
-            }
-
-            public Void MakeWithdrawal(decimal amount, DateTime date, string note)
-            {
-
-            }
-        }
-    }
